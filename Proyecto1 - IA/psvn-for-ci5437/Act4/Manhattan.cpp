b3ae5d140b7ce9f781cff2fc344f329984b788ce
rickyl22 <ricardolira48@hotmail.com>
2016-10-21 10:07:11 -0400

    Update Manhattan.cpp

--- Proyecto1 - IA/psvn-for-ci5437/Act4/Manhattan.cpp
+++ Proyecto1 - IA/psvn-for-ci5437/Act4/Manhattan.cpp
@@ -5,7 +5,41 @@
 
 using namespace std;
 
-// Distancias de Manhattan para 15-puzzle
+// Funcion heuristica Manhattan
+
+int manhatan(state_t state){
+	char stateArr[100];
+	sprint_state(stateArr,100,&state);
+	string vals(stateArr);
+	std :: istringstream iss(vals);
+        string curr;
+	iss >> curr;
+	int first;
+	int i =0;
+	int sum = 0;
+	
+	while(i<16){
+		if (curr == "B"){
+			if(i == 15)
+				break;
+			iss >> curr;
+			i++;
+			continue;
+		}
+		first = atoi(curr.c_str());
+		int row = first/4;
+		int col = first % 4;
+		int row2 = i/4;
+		int col2 = i % 4;
+		sum += abs(row - row2) + abs(col - col2);			
+		iss >> curr;
+		i++;
+		
+	}
+	return sum;
+}
+
+/* Distancias de Manhattan para 15-puzzle
 int mdistance[16][16] = 
 {
  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -60,4 +94,4 @@ int Manhattan(state_t state){
 	return distance;
 	
 }
-
+*/
