3a1d3fc67453fea6bffa86ea949b78c63aff1cb7
MrZyrus <kaiser705@gmail.com>
2016-12-11 22:23:33 -0400

    Oh yeah baby, it's all finished now

--- Proyecto3 - IA/decode.cc
+++ Proyecto3 - IA/decode.cc
@@ -11,6 +11,7 @@ using namespace std;
 int main(int argc, const char **argv) {
   int n, m;
   string line, aux;
+  ofstream outfile("solutions.txt");
   for (int filename = 1; filename < 63; filename++) {
     ifstream infile(to_string(filename) + ".txt");
     getline(infile, line);
@@ -18,28 +19,30 @@ int main(int argc, const char **argv) {
     iss >> aux;
     iss >> n;
     iss >> m;
-    ofstream outfile(to_string(filename) + "final.txt");
     for (int i = 2; i < line.length(); i++) {
       outfile << line[i];
     }
     outfile << '\n';
     infile.close();
+    remove((to_string(filename) + ".txt").c_str());
     ifstream infile2(to_string(filename) + "out.txt");
     getline(infile2, line);
-    outfile << n << ' ' << m << ' ';
+    outfile << n << ' ' << m;
     if (line == "SAT") {
       getline(infile2, line);
       istringstream iss(line);
       for (int i = 0; i < n*2+1; i++) {
+        outfile << ' ';
         for (int j = 0; j < m+i%2; j++) {
           iss >> aux;
           (aux[0] == '-') ? outfile << '0': outfile << '1';
         }
-        outfile << ' ';
       }
     }
-    if (line == "UNSAT") outfile << "UNSAT";
+    if (line == "UNSAT") outfile << " UNSAT";
+    outfile << '\n';
     infile2.close();
-    outfile.close();
+    remove((to_string(filename) + "out.txt").c_str());
   }
+  outfile.close();
 }
